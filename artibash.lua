d4ef70602d9b0c966c0ed710102ee996e051a2bc
maxthyme <simply.relativity@gmail.com>
2015-06-26 19:46:05 -0500

    Update artibash.lua

--- artibash.lua
+++ artibash.lua
@@ -1,13 +1,16 @@
 local unit = df.global.world.units.active[0]
 local attks = unit.actions
-for k in ipairs(attks) do
-	if attks[k].type==1 and attks[k].data.attack.attack_item_id then
-		local item = df.item.find(attks[k].data.attack.attack_item_id)
-		if item.flags.artifact == true then
-			for v = 0,1 do
-			item.subtype.attacks[v].velocity_mult = 2700000
-		attks[k].data.attack.unk_30=99999999
-		attks[k].data.attack.unk_3c=999
-		end
-	end
+        for k in ipairs(attks) do
+        if attks[k].type==1 and attks[k].data.attack.attack_item_id then
+                local item = df.item.find(attks[k].data.attack.attack_item_id)
+                        if item.flags.artifact == true then
+                        local acks = item.subtype.attacks
+                                for v in ipairs(acks)  do
+                                acks[v].velocity_mult = 2700000
+                        attks[k].data.attack.unk_30=99999999
+                        attks[k].data.attack.unk_3c=999
+                        end
+                end
+        end
 end
+
