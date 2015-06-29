2094b2951a4ab63ebd5c2ef30970e7365bf6de3e
maxthyme <simply.relativity@gmail.com>
2015-06-29 00:28:01 -0500

    Update artibash.lua

--- artibash.lua
+++ artibash.lua
@@ -1,3 +1,7 @@
+--The unk_30 and unk_3c effects were found by Putnam and Roses, respectively. 
+--They are attack_velocity and attack_accuracy.
+--This is just an early testing version to play around with.
+
 local unit = df.global.world.units.active[0]
 local attks = unit.actions
         for k in ipairs(attks) do
