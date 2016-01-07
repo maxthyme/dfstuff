bc7a6908747fe3610dbadbf2bfedfe266e05e63b
maxthyme <simply.relativity@gmail.com>
2016-01-06 22:26:28 -0600

    Update names.lua

--- names.lua
+++ names.lua
@@ -1,3 +1,16 @@
+--rename items or units
+--[[=begin
+
+names
+=====
+Rename units or items.  Usage:
+
+:-help:     print this help message
+:-item:     if viewing an item
+:-unit:     if viewing a unit
+:-first [Somename | "Some Names like This":
+            if a first name is desired, leave blank to clear current first name
+=end]]
 local utils = require 'utils'
 
 validArgs = validArgs or utils.invert({
@@ -13,43 +26,46 @@ if args.help then
  print(
 [[names.lua
 arguments:
-    -help				
+    -help
         print this help message
-    -item 					
-	if viewing an item		
-    -unit 					
-	if viewing a unit 			
+    -item
+    if viewing an item
+    -unit
+    if viewing a unit
     -first Somename or "Some Names like This"
-	if a first name is desired, leave blank to clear current first name
+    if a first name is desired, leave blank to clear current first name
 ]])
  return
 end
 
 if not df.viewscreen_layer_choose_language_namest:is_instance(dfhack.gui.getCurViewscreen()) then
-	choices = df.new(df.viewscreen_setupadventurest) ; choices.subscreen = 3 ; gui = require 'gui' ; gui.simulateInput(choices, 'A_RANDOM_NAME') ; gui.simulateInput(choices, 'A_CUST_NAME')
+    choices = df.viewscreen_setupadventurest:new() ; choices.page = 
+    df.viewscreen_setupadventurest.T_page.Background ; 
+    gui = 
+    require 'gui' ; gui.simulateInput(choices, 'A_RANDOM_NAME') ; gui.simulateInput(choices, 'A_CUST_NAME')
 end
 
 if args.item then
-	fact = dfhack.gui.getCurViewscreen().parent.item.general_refs[0].artifact_id
-	trg = df.artifact_record.find(fact)
+    fact = dfhack.gui.getCurViewscreen().parent.item.general_refs[0].artifact_id
+    trg = df.artifact_record.find(fact)
 end
  
 if args.unit then
-	trg = dfhack.gui.getCurViewscreen().parent.unit
+    trg = dfhack.gui.getCurViewscreen().parent.unit
 end
 
 if args.first then
-	trg.name.first_name = args.first
+    trg.name.first_name = args.first
 end
 
 function newName()
- 	newn = dfhack.gui.getCurViewscreen().name
-	oldn = trg.name
-	for k = 0,6 do
-	 	oldn.words[k] = newn.words[k]
- 		oldn.parts_of_speech[k] = newn.parts_of_speech[k]
-	end
- 	oldn.language = newn.language
- 	oldn.has_name = newn.has_name
+     newn = dfhack.gui.getCurViewscreen().name
+    oldn = trg.name
+    for k = 0,6 do
+         oldn.words[k] = newn.words[k]
+         oldn.parts_of_speech[k] = newn.parts_of_speech[k]
+    end
+     oldn.language = newn.language
+     oldn.has_name = newn.has_name
 end
 newName()
